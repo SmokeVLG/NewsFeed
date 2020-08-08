f2233c4df0cd41fb53cd9e544ecf4feed0d0022a
Maxim <denisov.mv@magnat.ru>
2020-08-08 13:41:49 +0400

    Add splash screen

---
+++ app/src/main/java/com/maxden/newsfeed/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.maxden.newsfeed
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+       /* startActivity(Intent(this, MainActivity::class.java))
+        finish()*/
+    }
+}
